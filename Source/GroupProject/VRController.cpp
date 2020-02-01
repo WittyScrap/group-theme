baefb77d3a541cb738894d125836f2777998b20c
Francesco Litrico <100492443@unimail.derby.ac.uk>
2020-02-01 15:02:43 +0000

    Level setup
    * Player start set
    * BP for base AVRController implemented
    * Default pawn set to new BP (Player)
    * More functionality added to AVRController

--- Source/GroupProject/VRController.cpp
+++ Source/GroupProject/VRController.cpp
@@ -2,6 +2,7 @@
 
 
 #include "VRController.h"
+#include "IXRTrackingSystem.h"
 
 // Sets default values
 AVRController::AVRController()
@@ -9,13 +10,40 @@ AVRController::AVRController()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	/* ------ Create VR components ------- */
+
+	VRRoot = CreateDefaultSubobject<USceneComponent>(TEXT("VR Root"));
+	VRRoot->SetupAttachment(GetRootComponent());
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("VR Camera"));
+	Camera->SetupAttachment(VRRoot);
+
+	/* ------- Create VR controllers --------- */
+
+	LeftHand = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("VR Left Hand"));
+	LeftHand->SetupAttachment(VRRoot);
+	LeftHand->SetTrackingSource(EControllerHand::Left);
+
+	RightHand = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("VR Right Hand"));
+	RightHand->SetupAttachment(VRRoot);
+	RightHand->SetTrackingSource(EControllerHand::Right);
+
+	if (LeftHand)
+	{
+		LeftHand->SetIsReplicated(true);
+	}
+
+	if (RightHand)
+	{
+		RightHand->SetIsReplicated(true);
+	}
 }
 
 // Called when the game starts or when spawned
 void AVRController::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -30,5 +58,51 @@ void AVRController::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+// Gets the current HMD connected to this device and used by this controller.
+IHeadMountedDisplay* AVRController::GetCurrentHMD()
+{
+	if (!VRHeadset)
+	{
+		if (!GEngine)
+		{
+			return NULL;
+		}
+
+		VRHeadset = GEngine->XRSystem->GetHMDDevice();
+	}
+
+	return VRHeadset;
+}
+
+// Gets the current position of various VR components.
+void AVRController::GetVRPositions(FTransform& head, FTransform& leftHand, FTransform& rightHand) const
+{
+	if (!GEngine)
+	{
+		head = FTransform::Identity;
+		leftHand = FTransform::Identity;
+		rightHand = FTransform::Identity;
+
+		return;
+	}
+
+	const auto& xrSystem = GEngine->XRSystem;
+
+	FTransform trackingToWorld = xrSystem->GetTrackingToWorldTransform();
+	FTransform headTracking;
+
+	FVector headTrackingLocation;
+	FQuat headTrackingRotation;
+
+	xrSystem->GetCurrentPose(IXRTrackingSystem::HMDDeviceId, headTrackingRotation, headTrackingLocation);
+
+	headTracking.SetLocation(headTrackingLocation);
+	headTracking.SetRotation(headTrackingRotation);
+
+	head = headTracking * trackingToWorld;
+	leftHand = FTransform(LeftHand->GetComponentRotation(), LeftHand->GetComponentLocation());
+	rightHand = FTransform(RightHand->GetComponentRotation(), RightHand->GetComponentLocation());
 }
 
--- Source/GroupProject/VRController.h
+++ Source/GroupProject/VRController.h
@@ -2,20 +2,65 @@
 
 #pragma once
 
+#include "Engine.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include "VRController.generated.h"
+#include "HeadMountedDisplay.h"
+#include "Camera/CameraComponent.h"
+#include "MotionControllerComponent.h"
+
+///////////////////////////////////////////
+#include "VRController.generated.h" /////// Leave last!
+///////////////////////////////////////////
 
 UCLASS()
 class GROUPPROJECT_API AVRController : public ACharacter
 {
 	GENERATED_BODY()
+		
+private:
+	/**
+	 * Print:
+	 * Allows for a log message to be printed to either the log
+	 * console or the screen.
+	 *
+	 */
+	static FORCEINLINE void Print(FString, bool = false, FColor = FColor::Green, float = 1.f);
+
+	// The VR headset that is used by this controller.
+	IHeadMountedDisplay* VRHeadset;
 
 public:
 	// Sets default values for this character's properties
 	AVRController();
 
 protected:
+	// Root component for VR controller subobjects.
+	UPROPERTY(EditDefaultsOnly, Category = "VR Components")
+	USceneComponent* VRRoot;
+
+	// Target camera
+	UPROPERTY(VisibleAnywhere, Category = "VR Components")
+	UCameraComponent* Camera;
+
+	// Used to specify the origin of the HMD
+	UPROPERTY(VisibleAnywhere, Category = "VR Components")
+	USceneComponent* VROrigin;
+
+	// Used to specify whether or not positional tracking will be used
+	UPROPERTY(EditDefaultsOnly, Category = "VR Settings")
+	bool bPositionalTracking;
+
+	////////////////////////
+	// Motion controllers //
+	////////////////////////
+
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	UMotionControllerComponent* LeftHand;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	UMotionControllerComponent* RightHand;
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
@@ -26,4 +71,33 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	// The HMD that is currently being used by this class
+	IHeadMountedDisplay* GetCurrentHMD();
+
+	// The positions of the main VR components.
+	UFUNCTION(BlueprintCallable, Category = "VR tools")
+	void GetVRPositions(FTransform& head, FTransform& lHand, FTransform& rHand) const;
 };
+
+// ---------------------------------
+// -- Inline function definitions --
+// ---------------------------------
+
+/**
+ * Print:
+ * Allows for a log message to be printed to either the log
+ * console or the screen.
+ *
+ */
+FORCEINLINE void AVRController::Print(FString text, bool log, FColor color, float time)
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, time, color, text);
+	}
+
+	if (log)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *text);
+	}
+}
