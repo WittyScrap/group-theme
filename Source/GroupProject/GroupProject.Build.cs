e680790657ce82b8e5b7612f6b00528264762be3
Francesco Litrico <100492443@unimail.derby.ac.uk>
2020-02-04 15:04:59 +0000

    Deleted VRController class
    RIP

--- Source/GroupProject/GroupProject.Build.cs
+++ Source/GroupProject/GroupProject.Build.cs
@@ -10,7 +10,7 @@ public class GroupProject : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "HeadMountedDisplay", "SteamVR" });
+		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
