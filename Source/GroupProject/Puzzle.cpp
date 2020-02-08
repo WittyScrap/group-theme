664368066dd5cb1c11032f58b9bb7d489d3f5305
Francesco <francescodglitrico@gmail.com>
2020-02-08 14:16:24 +0000

    Ported command (changeset)
    Forgot to stage changes last time sorry

--- Source/GroupProject/Puzzle.cpp
+++ Source/GroupProject/Puzzle.cpp
@@ -3,3 +3,40 @@
 
 #include "Puzzle.h"
 
+DEFINE_LOG_CATEGORY(GameLog);
+
+// Sets an in-game rule.
+void APuzzle::GameRule(const FName& ruleName, const int& value)
+{
+	SetRule(ruleName, value);
+
+	UE_LOG(GameLog, Log, TEXT("Set rule \"%s\" to value: %d"), *ruleName.ToString(), value);
+}
+
+// Sets an in-game rule.
+void APuzzle::SetRule(const FName& ruleName, const int& value)
+{
+	if (GameRules.Contains(ruleName))
+	{
+		GameRules[ruleName] = value;
+	}
+	else
+	{
+		GameRules.Add(ruleName, value);
+	}
+}
+
+// Tries getting a rule from the rule set.
+const bool APuzzle::GetRule(const FName& ruleName, int& value)
+{
+	int* rule = GameRules.Find(ruleName);
+
+	if (rule)
+	{
+		value = *rule;
+		return true;
+	}
+
+	value = 0;
+	return false;
+}
